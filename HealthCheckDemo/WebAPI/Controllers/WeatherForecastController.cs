42967a25d7a5b75ac239c8b3823d70f268d887bd
Administrator <admin@example.com>
2020-12-07 00:32:31 +0800

    update

--- HealthCheckDemo/WebAPI/Controllers/WeatherForecastController.cs
+++ HealthCheckDemo/WebAPI/Controllers/WeatherForecastController.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 
-namespace HealthCheckDemo.Controllers
+namespace WebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private readonly IDistributedCache _cache;
-
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -22,24 +18,14 @@ namespace HealthCheckDemo.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache cache)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
-            _cache = cache;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> Get()
+        public IEnumerable<WeatherForecast> Get()
         {
-            var key = "key";
-            var valueByte = await _cache.GetAsync(key);
-            if (valueByte == null)
-            {
-                await _cache.SetAsync(key, Encoding.UTF8.GetBytes("value"), new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(3000)));
-                valueByte = await _cache.GetAsync(key);
-            }
-            var valueString = Encoding.UTF8.GetString(valueByte);
-
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- HealthCheckDemo/WebAPI/Startup.cs
+++ HealthCheckDemo/WebAPI/Startup.cs
@@ -1,12 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using System;
+using Microsoft.Extensions.Logging;
 
-namespace HealthCheckDemo
+namespace WebAPI
 {
     public class Startup
     {
@@ -21,21 +26,6 @@ namespace HealthCheckDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            var redisString = "";
-            services.AddDistributedRedisCache(options =>
-            {
-                options.InstanceName = "";
-                options.Configuration = redisString;
-            });
-
-            var connectionString = Configuration.GetConnectionString("BloggingDatabase");
-            var weatherServiceUri = "https://localhost:44385";
-
-            services.AddHealthChecks()
-                    .AddSqlServer(connectionString)
-                    .AddRedis(redisString)
-                    .AddUrlGroup(new Uri($"{weatherServiceUri}/weatherforecast"), "Weather API Health Check", HealthStatus.Degraded, timeout: new System.TimeSpan(0,0,3));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,7 +45,6 @@ namespace HealthCheckDemo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health");
             });
         }
     }
